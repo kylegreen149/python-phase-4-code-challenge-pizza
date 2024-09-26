8f917819cd811dee7805f9504524a6f482a86f71
Kyle Greenidge <kylegreen149@gmail.com>
2024-09-26 15:56:00 -0400

    Done

--- server/app.py
+++ server/app.py
@@ -24,6 +24,46 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route("/restaurants", methods=["GET"])
+def get_all_restaurants():
+    if request.method == 'GET':
+        return [r.to_dict(rules=['-restaurant_pizzas']) for r in Restaurant.query.all()], 200
+
+@app.route("/restaurants/<int:id>", methods=["GET", "DELETE"])
+def get_restaurant_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if restaurant is None:
+        return {"error": "Restaurant not found"}, 404
+    if request.method == 'GET':
+        return restaurant.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        return {}, 204
+    
+
+@app.route("/pizzas", methods=["GET"])
+def get_all_pizzas():
+    if request.method == 'GET':
+        return [p.to_dict(rules=['-restaurant_pizzas']) for p in Pizza.query.all()], 200
+
+@app.route("/restaurant_pizzas", methods=["POST"])
+def add_price():
+    # if request.method == 'GET':
+    #     return [rp.to_dict(rules=['-pizza', '-restaurant']) for rp in RestaurantPizza.query.all()], 200
+    if request.method == 'POST':
+        data = request.get_json()
+        try:
+            add_price = RestaurantPizza(
+                price=data.get('price'),
+                restaurant_id=data.get('restaurant_id'),
+                pizza_id=data.get('pizza_id')
+            )
+        except ValueError:
+            return {"errors": ["validation errors"]}, 400
+        db.session.add(add_price)
+        db.session.commit()
+        return add_price.to_dict(), 201
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,8 +21,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurants',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +38,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +52,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
 
     # add relationships
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_rules = ('-restaurants.restaurant_pizzas', '-pizza.restaurant_pizzas',)
 
     # add validation
+    @validates('price')
+    def valid_price(self, key, new_value):
+        if 1 <= new_value <= 30:
+            return new_value
+        else:
+            raise ValueError(f'{key} must be between 1 and 30')
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
